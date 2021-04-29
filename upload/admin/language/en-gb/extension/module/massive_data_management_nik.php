c54eb57f8f03f8d7d2c86a197d881927ce77bdf4
Ernie <ernst@jacob.ch>
2021-04-29 14:28:44 +0000

    Update massive_data_management_nik.php

--- upload/admin/language/en-gb/extension/module/massive_data_management_nik.php
+++ upload/admin/language/en-gb/extension/module/massive_data_management_nik.php
@@ -1,1 +1,27 @@
+<?php
+// Heading
+$_['heading_title'] = 'Mass Data Management';
 
+// Text
+$_['text_extension'] = 'Extensions';
+$_['text_success'] = 'Settings are successfully changed!';
+$_['text_edit'] = 'Module Settings';
+$_['text_customers'] = 'Clients';
+$_['text_categories'] = 'Categories';
+$_['text_products'] = 'Products';
+$_['text_all'] = 'all';
+$_['text_number'] = 'Number';
+$_['text_percent'] = 'Percentage';
+$_['text_plus'] = '+';
+$_['text_minus'] = '-';
+$_['text_equal'] = '=';
+
+// Entry
+$_['entry_products_list'] = 'List of Goods';
+$_['entry_status'] = 'Status';
+
+// Buttons
+$_['button_apply_changes'] = 'Apply the changes';
+
+// Error
+$_['error_permission'] = 'You do not have the right to change the Mass Management Module!';
